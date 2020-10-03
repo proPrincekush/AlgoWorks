f853e232d6bb8692e3bece4b8f5f2d59153bd9c7
proprincekush <princetechspacer@gmail.com>
2020-10-03 09:35:16 +0530

    fibnacci problem solved

---
+++ fib/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n) {
+    fibSeries = [0,1]
+       for(let i = 2; i<=n;i++){
+             fibSeries[i] = fibSeries[i-1]+fibSeries[i-2]
+       }
+    return fibSeries[n]
+}
+
+// recursive solution
+// function fib(n) {
+
+//     if (n<2) {
+//        return n
+//     }
+      
+//     return fib(n-1)+fib(n-2)  
+//   }
+module.exports = fib;
