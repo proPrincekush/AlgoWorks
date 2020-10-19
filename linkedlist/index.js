80cd4b9865a574c51eaa37b227d73689d65248ce
Jai Mata Di <jaimatadi@Jais-iMac.local>
2020-10-19 11:51:53 +0530

    solved some linked list problem

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -13,6 +13,51 @@ class LinkedList {
     constructor(){
         this.head = null;
     }
+
+    insertFirst(data){
+        this.head =  new Node(data,this.head);
+    }
+
+    size(){
+        let i = this.head;
+        let count = 0;
+        while(i){
+            count++;
+            i=i.next
+        }
+        return count;
+    }
+
+    getFirst(){
+        return this.head
+    }
+
+    getLast(){
+        let i = this.head;
+        while(i){
+            if (!i.next) {
+                return i
+            } else {
+                i=i.next 
+            }
+        }
+        return null;
+    }
+
+    clear(){
+        this.head = null;
+    }
+
+    removeFirst(){
+        if (this.head) {
+            this.head = this.head.next
+        } else {
+            return;
+        } 
+    }
+
+
 }
+
 
 module.exports = { Node, LinkedList };
